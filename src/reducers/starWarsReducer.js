f8720ff03ffc4f8411d251755d2be3ad34a40be7
adam <bcuz@users.noreply.github.com>
2019-04-16 21:23:19 -0400

    char names rendering

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,6 @@
-import /* we need our action types here*/ "../actions";
+import {SUCCESS} from "../actions";
 const initialState = {
-  characters: [{name: 'bob'}]
+  characters: []
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +8,12 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case SUCCESS:
+      return {
+        ...state,
+        characters: action.characters
+        // isLoading: false,
+      };
     default:
       return state;
   }
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import {getChars} from "../actions";
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -11,6 +12,7 @@ class CharacterListView extends React.Component {
 
   componentDidMount() {
     // call our action
+    this.props.getChars()
   }
 
   render() {
@@ -35,5 +37,6 @@ export default connect(
   mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getChars
   }
 )(CharacterListView);
